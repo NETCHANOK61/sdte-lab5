f35e404c84dcb98bfa97a24e3eade833a0cdeb13
Nam <61070106@it.kmitl.ac.th>
2021-02-15 18:12:21 +0700

    lab5

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const server = require('./server')
 const port = 3000
 
 server.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
